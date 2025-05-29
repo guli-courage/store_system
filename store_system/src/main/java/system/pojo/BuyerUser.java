0b850d877ba9dbb06eba1589ee08f0871dfa76ee
guli-courage <3437029044@qq.com>
2025-05-29 20:03:33 +0800

    登录重新整合

---
+++ store_system/src/main/java/system/pojo/BuyerUser.java
@@ -0,0 +1,27 @@
+package system.pojo;
+
+import lombok.Data;
+
+@Data
+
+public class BuyerUser {
+//    user_id	varchar
+//    username	varchar
+//    password	varchar
+//    gender	varchar
+//    phone	varchar
+//    nickname	varchar
+//    avatar	varchar
+//    open_id	varchar
+//    address	varchar
+    Integer userId;
+    String userName;
+    String password;
+    String gender;
+    String phone;
+    String nickname;
+    String avatar;
+    String openid;
+    String address;
+    
+}
--- store_system/src/main/java/system/service/LoginService.java
+++ store_system/src/main/java/system/service/LoginService.java
@@ -3,6 +3,8 @@ package system.service;
 
 import cn.hutool.core.lang.UUID;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class LoginService {
 
@@ -48,19 +51,27 @@ public class LoginService {
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type=authorization_code";
         String replaceUrl = url.replace("{0}", appid).replace("{1}", secret).replace("{2}",wxAuth.getCode());
         String res = HttpUtil.get(replaceUrl);
+        System.out.println("res:"+res);
+        System.out.println();
         String uuid = UUID.randomUUID().toString();
         stringRedisTemplate.opsForValue().set(RedisKey.WX_SESSION_ID+uuid, res);
         wxAuth.setSessionId(uuid);
-        System.out.println(uuid);
+        System.out.println("sessionID:"+uuid);
         try {
-
-
             String wxRes = wxService.wxDecrypt(wxAuth.getEncryptedData(), wxAuth.getSessionId(), wxAuth.getIv());
+            log.info("用户信息："+wxRes);
             //用户信息：{"openId":"o20","nickName":"juana","gender":2,"language":"zh_CN","city":"Changsha","province":"Hunan","country":"China","avatarUrl":"头像链接","watermark":{"timestamp":dsfs,"appid":"应用id"}}
             WxUserInfo wxUserInfo = JSON.parseObject(wxRes,WxUserInfo.class);
+            //由于获取用户数据不再拿到openId,所以直接调用redis，从其中获取
+            String json = stringRedisTemplate.opsForValue().get(RedisKey.WX_SESSION_ID + uuid);
+            JSONObject jsonObject = JSON.parseObject(json);
+            String openid = (String) jsonObject.get("openid");
+            wxUserInfo.setOpenid(openid);
             // 业务操作：你可以在这里利用数据 对数据库进行查询， 如果数据库中没有这个数据，就添加进去，即实现微信账号注册
-            System.out.println(wxUserInfo.getOpenid());
+            System.out.println(wxUserInfo);
             // 如果是已经注册过的，就利用数据，生成jwt 返回token，实现登录状态
+
+
             return Result.SUCCESS(wxUserInfo);
         } catch (Exception e) {
             e.printStackTrace();
